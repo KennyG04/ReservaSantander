beb7d62d1a9fe2687dc69711177b5bcfa7b25092
ESPE <ESPE@LAB06-PC20.mshome.net>
2024-07-29 08:37:52 -0500

    Ejemplo label

--- src/Interfaces_Recepcionista/Principal_Hotel.java
+++ src/Interfaces_Recepcionista/Principal_Hotel.java
@@ -111,9 +111,7 @@ public class Principal_Hotel extends javax.swing.JFrame {
         Days = new javax.swing.JSlider();
         SliderDaysInfo = new javax.swing.JLabel();
         jLabel46 = new javax.swing.JLabel();
-        OutDate = new com.toedter.calendar.JDateChooser();
         jLabel47 = new javax.swing.JLabel();
-        BeginingDate = new com.toedter.calendar.JDateChooser();
         jLabel48 = new javax.swing.JLabel();
         jLabel49 = new javax.swing.JLabel();
         jLabel50 = new javax.swing.JLabel();
@@ -158,8 +156,6 @@ public class Principal_Hotel extends javax.swing.JFrame {
         SendDataToBill = new javax.swing.JButton();
         RoomCodeTF = new javax.swing.JTextField();
         DaysSliderRoom = new javax.swing.JSlider();
-        jDateChooser1 = new com.toedter.calendar.JDateChooser();
-        jDateChooser2 = new com.toedter.calendar.JDateChooser();
         DaysRoom = new javax.swing.JLabel();
         jLabel76 = new javax.swing.JLabel();
         RoomTypeCB = new javax.swing.JComboBox<>();
@@ -244,10 +240,6 @@ public class Principal_Hotel extends javax.swing.JFrame {
         jButton23 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         RoomsTypeFilter = new javax.swing.JComboBox<>();
-        Room6Panel1 = new test.PanelRound();
-        RoomAvaliability11 = new javax.swing.JLabel();
-        jButton24 = new javax.swing.JButton();
-        RoomCode11 = new javax.swing.JLabel();
         InformationPanel = new javax.swing.JPanel();
         SimpleRoomL = new javax.swing.JLabel();
         SimpleRoomL1 = new javax.swing.JLabel();
@@ -410,11 +402,9 @@ public class Principal_Hotel extends javax.swing.JFrame {
 
         jLabel46.setText("Fecha de salida");
         QuoteDialog.getContentPane().add(jLabel46, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 350, -1, -1));
-        QuoteDialog.getContentPane().add(OutDate, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 380, 220, -1));
 
         jLabel47.setText("Fecha de ingreso");
         QuoteDialog.getContentPane().add(jLabel47, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 250, -1, -1));
-        QuoteDialog.getContentPane().add(BeginingDate, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 290, 220, -1));
 
         jLabel48.setText("Total de noches");
         QuoteDialog.getContentPane().add(jLabel48, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 90, -1, -1));
@@ -585,8 +575,6 @@ public class Principal_Hotel extends javax.swing.JFrame {
             }
         });
         RoomDataDialog.getContentPane().add(DaysSliderRoom, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 120, 210, -1));
-        RoomDataDialog.getContentPane().add(jDateChooser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 190, 210, -1));
-        RoomDataDialog.getContentPane().add(jDateChooser2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 150, 210, -1));
 
         DaysRoom.setText("0 dias");
         RoomDataDialog.getContentPane().add(DaysRoom, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 120, -1, -1));
@@ -1099,35 +1087,6 @@ public class Principal_Hotel extends javax.swing.JFrame {
             }
         });
         RoomsPanel.add(RoomsTypeFilter, new org.netbeans.lib.awtextra.AbsoluteConstraints(900, 70, 100, -1));
-
-        Room6Panel1.setBackground(new java.awt.Color(177, 213, 200));
-        Room6Panel1.setForeground(new java.awt.Color(255, 255, 255));
-        Room6Panel1.setRoundBottomLeft(30);
-        Room6Panel1.setRoundBottomRight(30);
-        Room6Panel1.setRoundTopLeft(30);
-        Room6Panel1.setRoundTopRight(30);
-        Room6Panel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        RoomAvaliability11.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        RoomAvaliability11.setText("Disponible");
-        Room6Panel1.add(RoomAvaliability11, new org.netbeans.lib.awtextra.AbsoluteConstraints(16, 200, 150, -1));
-
-        jButton24.setBackground(new java.awt.Color(73, 114, 116));
-        jButton24.setForeground(new java.awt.Color(255, 255, 255));
-        jButton24.setText("Rentar");
-        jButton24.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton24ActionPerformed(evt);
-            }
-        });
-        Room6Panel1.add(jButton24, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 220, 160, 20));
-
-        RoomsPanel.add(Room6Panel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 680, 180, 370));
-
-        RoomCode11.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        RoomCode11.setText("a123");
-        RoomCode11.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        RoomsPanel.add(RoomCode11, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 850, 160, -1));
 
         getContentPane().add(RoomsPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 0, 1570, 1080));
 
@@ -1964,10 +1923,6 @@ public class Principal_Hotel extends javax.swing.JFrame {
             setDataOnRoomPanels(3);
         }
     }//GEN-LAST:event_RoomsTypeFilterActionPerformed
-
-    private void jButton24ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton24ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton24ActionPerformed
     
     public void generateJSONTempCredentials(){
         // Crear un objeto JSON para almacenar las credenciales
@@ -2379,7 +2334,6 @@ public class Principal_Hotel extends javax.swing.JFrame {
     private javax.swing.JButton AdditionalsB;
     private javax.swing.JPanel AdditionalsPanel;
     private javax.swing.JDialog AsignCleaningDialog;
-    private com.toedter.calendar.JDateChooser BeginingDate;
     private javax.swing.JTable BillDescriptionTable;
     private javax.swing.JPanel CleaningPanel;
     private javax.swing.JButton CleanningB;
@@ -2432,7 +2386,6 @@ public class Principal_Hotel extends javax.swing.JFrame {
     private javax.swing.JLabel MatrimonialRoomL;
     private javax.swing.JTextField NameTF;
     private javax.swing.JTextField Nights;
-    private com.toedter.calendar.JDateChooser OutDate;
     private javax.swing.JLabel PersonalRoomL;
     private javax.swing.JTextField PhoneTF;
     private javax.swing.JDialog QuoteDialog;
@@ -2451,7 +2404,6 @@ public class Principal_Hotel extends javax.swing.JFrame {
     private test.PanelRound Room5Panel;
     private javax.swing.JLabel Room6Image;
     private test.PanelRound Room6Panel;
-    private test.PanelRound Room6Panel1;
     private javax.swing.JLabel Room7Image;
     private test.PanelRound Room7Panel;
     private javax.swing.JLabel Room8Image;
@@ -2460,7 +2412,6 @@ public class Principal_Hotel extends javax.swing.JFrame {
     private test.PanelRound Room9Panel;
     private javax.swing.JLabel RoomAvaliability1;
     private javax.swing.JLabel RoomAvaliability10;
-    private javax.swing.JLabel RoomAvaliability11;
     private javax.swing.JLabel RoomAvaliability2;
     private javax.swing.JLabel RoomAvaliability3;
     private javax.swing.JLabel RoomAvaliability4;
@@ -2471,7 +2422,6 @@ public class Principal_Hotel extends javax.swing.JFrame {
     private javax.swing.JLabel RoomAvaliability9;
     private javax.swing.JLabel RoomCode1;
     private javax.swing.JLabel RoomCode10;
-    private javax.swing.JLabel RoomCode11;
     private javax.swing.JLabel RoomCode2;
     private javax.swing.JLabel RoomCode3;
     private javax.swing.JLabel RoomCode4;
@@ -2529,7 +2479,6 @@ public class Principal_Hotel extends javax.swing.JFrame {
     private javax.swing.JButton jButton21;
     private javax.swing.JButton jButton22;
     private javax.swing.JButton jButton23;
-    private javax.swing.JButton jButton24;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton31;
     private javax.swing.JButton jButton32;
@@ -2549,8 +2498,6 @@ public class Principal_Hotel extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JComboBox<String> jComboBox3;
     private javax.swing.JComboBox<String> jComboBox4;
-    private com.toedter.calendar.JDateChooser jDateChooser1;
-    private com.toedter.calendar.JDateChooser jDateChooser2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
